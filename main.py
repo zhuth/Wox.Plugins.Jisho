2b3e54a8fbe7b58ac5817948860a74813dfc75a5
zhuth <zthpublic@gmail.com>
2019-09-01 09:24:40 +0800

    init

---
+++ main.py
@@ -0,0 +1,54 @@
+import requests
+from bs4 import BeautifulSoup as B
+import pyperclip
+
+try:
+    from wox import Wox, WoxAPI
+except ImportError:
+    import sys
+    sys.path.insert(0, r'..\..\..\..\Local\Wox\app-1.3.524\JsonRPC')
+    from wox import Wox, WoxAPI
+
+
+class Main(Wox):
+
+    def request_jisho(self, key):
+        html = requests.get(f'https://jisho.org/search/{key}').content
+        b = B(html, 'lxml')
+        return b
+
+    def meaning(self, b):
+        return '; '.join([d.text.strip() for d in b.find_all(class_='meaning-definition')])
+
+    def title(self, b):
+        return b.find(class_='text').text.strip()
+
+    def parse_results(self, b):
+        return [
+            {
+                "Title": self.title(item),
+                "SubTitle": self.meaning(item),
+                "IcoPath": "Images\\icon.png",
+                "JsonRPCAction": {
+                    "method": "copy",
+                    "parameters": [self.title(item)],
+                    "dontHideAfterAction": False
+                }
+            }
+            for item in b.find_all(class_='concept_light')
+            if item.find(class_='text')
+        ]
+
+    def query(self, key):
+        if key:
+            b = self.request_jisho(key)
+            return self.parse_results(b)
+        else:
+            return []
+
+    def copy(self, text):
+        pyperclip.copy(text)
+
+
+if __name__ == "__main__":
+    Main()
